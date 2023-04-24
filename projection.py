a7c6d9d945437affbc05de116fbb2683a3a54832
Lucas.Goh <leftcompact@gmail.com>
2023-04-24 21:28:35 +0900

    draw boards

---
+++ projection.py
@@ -0,0 +1,30 @@
+import pygame
+import numpy as np
+import math
+
+WHITE = (255, 255, 255)
+RED = (255, 0 ,0)
+BLACK = (0, 0, 0)
+
+WIDTH, HEIGHT = 800, 600
+pygame.init()
+pygame.display.set_caption("3D projection in pygame!")
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                exit()
+
+
+    # update stuff
+
+    screen.fill(WHITE)
+    # drawing stuff
+
+    pygame.display.update()
