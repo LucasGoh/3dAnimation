913914cf44ef346edc196e109ac27d63f7187db2
Lucas.Goh <leftcompact@gmail.com>
2023-04-24 22:04:44 +0900

    draw square on screen

--- projection.py
+++ projection.py
@@ -11,6 +11,24 @@ pygame.init()
 pygame.display.set_caption("3D projection in pygame!")
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
+scale = 100
+
+circle_pos = [WIDTH/2, HEIGHT/2]
+points = []
+
+points.append(np.matrix([-1, -1, 1]))
+points.append(np.matrix([1, -1, 1]))
+points.append(np.matrix([1,  1, 1]))
+points.append(np.matrix([-1, 1, 1]))
+points.append(np.matrix([-1, -1, -1]))
+points.append(np.matrix([1, -1, -1]))
+points.append(np.matrix([1, 1, -1]))
+points.append(np.matrix([-1, 1, -1]))
+
+projection_matrix = np.matrix([
+    [1, 0, 0],
+    [0, 1, 0]
+])
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -26,5 +44,12 @@ while True:
 
     screen.fill(WHITE)
     # drawing stuff
+
+    for point in points:
+        projected2d = np.dot(projection_matrix, point.reshape((3, 1)))
+
+        x = int(projected2d[0][0] * scale) + circle_pos[0]
+        y = int(projected2d[1][0] * scale) + circle_pos[1]
+        pygame.draw.circle(screen, BLACK, (x,y), 5)
 
     pygame.display.update()
